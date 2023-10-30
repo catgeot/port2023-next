fb7c89dc362772caea00607f52b55e5f51531d1d
catgeot <catgeot@naver.com>
2023-10-31 08:06:30 +0900

    인트로 완료

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,49 @@
+import { introText } from '@/constants'
+import Image from 'next/image'
 import React from 'react'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <section id="intro">
+            <div class="intro_inner">
+                <h2 class="intro_title">
+                    port developer
+                </h2>
+               <div class="intro_lines" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+               </div>
+               <div class="intro_text">
+                    <div class="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div class="img">
+                        <Image
+                          src={introText.img}
+                          alt='about'
+                          width={300}
+                          height={300}
+                        />
+                    </div>
+               </div> 
+               <div class="intro_lines bottom" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+           </div>
+            </div>
+        </section>
   )
 }
 
